317b40d518aece3bb0df842a9b25700b0709f704
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-17 08:47:44 +0300

    l 03

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pageRoute = require("./routes/pageRoute");
 
 const app = express();
 
@@ -9,17 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Routes
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
+app.use("/", pageRoute);
 
 const port = 3000;
 app.listen(port, () => {

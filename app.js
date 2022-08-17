8896139408c5938f4a46e9609ce5748bc8ef604b
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-17 09:24:18 +0300

    l 04

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
 
 const app = express();
+
+//Connect DB
+mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
+  console.log("DB Connected Successfuly");
+});
 
 //Template Engine
 app.set("view engine", "ejs");
@@ -11,6 +18,7 @@ app.use(express.static("public"));
 
 //Routes
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {

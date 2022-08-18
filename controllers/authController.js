109a33eb26b4037d670cde87a0372d7bd1852848
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-18 08:49:33 +0300

    l 12

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ exports.loginUser = async (req, res) => {
         if (same) {
           // User Session
           req.session.userID = user._id;
-          res.status(200).redirect("/");
+          res.status(200).redirect("/users/dashboard");
         }
       });
     }
@@ -40,5 +40,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect("/");
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    user,
   });
 };

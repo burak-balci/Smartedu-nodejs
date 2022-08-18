bf04832176fcded27d12f1d70ec2b9f8677752fb
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-18 08:02:01 +0300

    l 10 session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
@@ -15,12 +16,26 @@ mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
 //Template Engine
 app.set("view engine", "ejs");
 
+//Global Variable
+global.userIN = null;
+
 //Middlewares
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //Routes
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,8 @@ exports.loginUser = async (req, res) => {
       bcrypt.compare(password, user.password, (err, same) => {
         if (same) {
           // User Session
-          res.status(200).send("You are logged in");
+          req.session.userID = user._id;
+          res.status(200).redirect("/");
         }
       });
     }

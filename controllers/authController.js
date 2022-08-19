6f0bb70e04be2299f8f4fb0fb8ffac3d13b4de0f
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-19 10:49:07 +0300

    l 18

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,11 +21,9 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) {
       bcrypt.compare(password, user.password, (err, same) => {
-        if (same) {
-          // User Session
-          req.session.userID = user._id;
-          res.status(200).redirect("/users/dashboard");
-        }
+        // User Session
+        req.session.userID = user._id;
+        res.status(200).redirect("/users/dashboard");
       });
     }
   } catch (error) {
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -10,5 +10,6 @@ router
 router.route("/").get(courseController.getAllCourses);
 router.route("/:slug").get(courseController.getCourse);
 router.route("/enroll").post(courseController.enrollCourse);
+router.route("/release").post(courseController.releaseCourse);
 
 module.exports = router;
